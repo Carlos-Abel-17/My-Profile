31e87a7bc59a381b5a7f25aedee845788c88d698
Abel Aguado Ramos <135867557+Carlos-Abel-17@users.noreply.github.com>
2023-11-21 00:22:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Bootstap from './img/Bootstrap.png'
 import JScrip from './img/JavaScript.png'
 import material from './img/material.svg'
 import react from './img/react.png'
-import Vite from './img/Vite.png'
+import vites from './img/Vite.png'
 import tailwind from './img/Tailwind.png'
 
 import sql from './img/mysql.png'
@@ -27,7 +27,7 @@ function App() {
     imagenes:[JScrip,sql,node,postgres,prisma,seque,typescript],
     nombres:['JavaScript','Mysql','Node.js','Postgresql','Prisma','Sequelize','Typescript']}
   const fotoFront={
-    imagenes:[Bootstap,JScrip,material,react,Vite,tailwind],nombres:['Bootstap','JavaScript','Material ui','React','Vite','Tailwindcss']
+    imagenes:[Bootstap,JScrip,material,react,vites,tailwind],nombres:['Bootstap','JavaScript','Material ui','React','Vite','Tailwindcss']
   }
  
 
